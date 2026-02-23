4510040c24d848b4f307d49c10850a352c073226
pehyanbin <pehyanbin@gmail.com>
2026-02-24 00:21:33 +0800

    UI redesign

--- MIR/obj/Debug/net8.0-windows/Views/ReportsView.g.cs
+++ MIR/obj/Debug/net8.0-windows/Views/ReportsView.g.cs
@@ -52,7 +52,7 @@ namespace MIR.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MIR;V1.0.0.0;component/views/reportsview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MIR;component/views/reportsview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ReportsView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
